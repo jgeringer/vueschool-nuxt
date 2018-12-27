02d880ea455cca9ed716e783cefed5439347f168
Joseph P Geringer <joegeringer@gmail.com>
2018-12-26 23:50:32 -0600

    did the tut

---
+++ vueschool-nuxt/components/TheNavbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="nav">
+        <div class="log">
+            <nuxt-link :to="{ name: 'index' }" class="logo text-lg">Nuxt Fundamentals</nuxt-link>
+            <span class="subheader">A Vue School Course</span>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vueschool-nuxt/store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+    all: [
+        {
+            id: 'balut',
+            title: 'what is balut?',
+            content: 'this is the answer'
+        },
+        {
+            id: 'balut2',
+            title: 'what is balut second?',
+            content: 'this is the answer second'
+        },
+        {
+            id: 'balut3',
+            title: 'what is balut third?',
+            content: 'this is the answer third'
+        }
+    ]
+})
